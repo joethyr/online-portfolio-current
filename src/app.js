068aafa377be20b4d402d8799b2c6be0769e2f9f
joethyr <joethayr@gmail.com>
2023-04-19 08:09:12 -0600

    add about me section.

--- src/app.js
+++ src/app.js
@@ -6,19 +6,12 @@ $(".slider").slick({
   infinite: true,
   responsive: [
     {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 800,
+      breakpoint: 992,
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2,
+        infinite: true,
+        dots: true,
       },
     },
     {
