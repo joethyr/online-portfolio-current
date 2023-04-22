b8c5e76d34355e18394b111e5cef64c1e4b69c49
joethyr <joethayr@gmail.com>
2023-04-22 15:33:12 -0600

    update title and name in header

--- src/app.js
+++ src/app.js
@@ -21,8 +21,5 @@ $(".slider").slick({
         slidesToScroll: 1,
       },
     },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ],
 });
